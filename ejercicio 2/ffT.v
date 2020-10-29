f61f9d87f22869934e7f3797bafcd1b934f8a314
pat19218 <68085020+pat19218@users.noreply.github.com>
2020-10-29 13:30:47 -0600

    2do

---
+++ ejercicio 2/ffT.v
@@ -0,0 +1,31 @@
+module ffd1(input clk, input reset, input ena, input d, output logic q);
+
+	always @ (posedge clk, posedge reset)
+		if (reset)
+			q <= 1'b0;
+		else if (ena)
+			q <= ~d;			
+endmodule
+
+module ffd2(input clk, input reset, input ena, input [1:0]d, output logic [1:0]q);
+
+	always @ (posedge clk, posedge reset)
+		if (reset)
+			q <= 2'b0;
+		else if (ena)
+			q <= ~d;			
+endmodule
+
+module ffd4(input clk, input reset, input ena, input [3:0]d, output logic [3:0]q);
+
+	always @ (posedge clk, posedge reset)
+		if (reset)
+			q <= 4'b0;
+		else if (ena)
+			q <= ~d;			
+endmodule
+
+
+
+
+
---
+++ ejercicio 2/ffT_tb.v
@@ -0,0 +1,45 @@
+module testbench();
+
+reg clk = 0;
+reg reset = 0;
+reg enabled = 0;
+reg d1;
+reg [1:0]d2;
+reg [3:0]d4;
+output wire q1;
+output wire[1:0]q2;
+output wire[3:0]q4;
+
+ffd1 eins(clk, reset, enabled, d1, q1);
+ffd2 zweis(clk, reset, enabled, d2, q2);
+ffd4 vier(clk, reset, enabled, d4, q4);
+
+initial begin
+     #1	d1=1'b1; d2=2'b10;  d4=4'b1010;
+     #1 d1=1'b0; d2=2'b00;  d4=4'b1110; 
+     #1 d1=1'b1; d2=2'b01;  d4=4'b1011; enabled=1;
+     #1 d1=1'b1; d2=2'b11;  d4=4'b1100; #1 reset=1; #3 reset=0;
+     #1 d1=1'b0; d2=2'b10;  d4=4'b1111;
+     #1 d1=1'b1; d2=2'b11;  d4=4'b0001; enabled=0;
+     #1 d1=1'b0; d2=2'b11;  d4=4'b1110; enabled=1;
+     #1 d1=1'b1; d2=2'b10;  d4=4'b1101;
+
+end
+
+
+
+always@(*)begin
+	#1 clk = ~clk;
+end
+always@(*)begin
+	#1 clk = ~clk;
+end
+
+initial
+	#20 $finish;		
+
+initial begin
+	$dumpfile("ffT_tb.vcd");
+	$dumpvars(0, testbench);
+	end
+endmodule
